20d14f1ec9c4e712ac8460df407d7085c00b2ff3
ALEX321616 <86130136+ALEX321616@users.noreply.github.com>
2022-04-01 19:15:36 +0000

    Deploying to gh-pages from @ ALEX321616/goit-react-hw-05-movies@ea9df46ecc259c5918d61497129b4f22670de8dc 🚀

---
+++ static/js/566.907fe11e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[566],{3566:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var o=t(501),r=t(184);function i(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(o.rU,{to:"/",children:" Go to Home "}),(0,r.jsx)("div",{className:"NotFoundView",children:"404 Page not found"})]})}}}]);
+//# sourceMappingURL=566.907fe11e.chunk.js.map
